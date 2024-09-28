622b5a0048e42878715ba084b87a95c150bc691b
Brbzdum <vladimirbandenkov@gmail.com>
2024-09-28 12:36:07 +0300

    gavno

--- frontend/src/VideoCard.js
+++ frontend/src/VideoCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './VideoCard.css';
 
-function VideoCard({ image, title, channel, views, timestamp }) {
+function VideoCard({ image, title, channel, description, timestamp, like, dislike }) {
   return (
     <div className="videoCard">
       <img className="videoCard__thumbnail" src={image} alt={title} />
       <div className="videoCard__info">
         <h4>{title}</h4>
         <p>{channel}</p>
-        <p>{views} • {timestamp}</p>
+        <p>{description} • {timestamp}</p>
+        <button className="videoCard__likes">{like} Like</button>
+        <button className="videoCard__dislikes">{dislike} Dislike</button>
       </div>
     </div>
   );
--- frontend/src/VideoGrid.js
+++ frontend/src/VideoGrid.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoCard from './VideoCard';
 import './VideoGrid.css';
+import foto from './assets/fota.jpg';
 
 function VideoGrid() {
-  const imageUrl = "https://images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/19403ebe26a46a077e309a07d382cd875a5929ab8bc7585e04e084a1f3462cb21602502290105.jpg";
 
   return (
     <div className="videoGrid">
@@ -12,61 +12,64 @@ function VideoGrid() {
         title="Пример видео"
         timestamp="3 дня назад"
         channel="Канал 1"
-        image={imageUrl}
+        description="Смотрите на канале Канал ПИЗДЮКИ ПИЗДЯТСЯ ЗА ЧЕКУШКУ" 
+        image={foto}
+
       />
       <VideoCard
         title="Видео 2"
         timestamp="2 недели назад"
         channel="Канал 2"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title=" на свой канал"
         timestamp="2 недели назад"
         channel="Канал 3"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title="САША КЕРПЛ"
         timestamp="2 недели назад"
         channel="Канал 4"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title="Видео 5"
         timestamp="1 месяц назад"
         channel="Канал 5"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title="Видео 6"
         timestamp="1 месяц назад"
         channel="Канал 6"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title="Видео 7"
         timestamp="3 дня назад"
         channel="Канал 7"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title="Видео 8"
         timestamp="4 дня назад"
         channel="Канал 8"
-        image={imageUrl}
+        image={foto}
       />
       <VideoCard
         title="Видео 9"
         timestamp="5 дней назад"
         channel="Канал 9"
-        image={imageUrl}
+        description="ПРИМЕР "
+        image={foto}
       />
       <VideoCard
         title="Видео 10"
         timestamp="6 дней назад"
         channel="Канал 10"
-        image={imageUrl}
+        image={foto}
       />
     </div>
   );
